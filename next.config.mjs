e4e4084cec8a21d27ba866cfdacac10beb596457
Sergey <info@rou.digital>
2024-08-11 14:07:14 +0300

    Code styling

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    output: 'standalone',
+    sassOptions: {
+        includePaths: [path.join(path.dirname(fileURLToPath(import.meta.url)), 'styles')],
+    },
+    images: {
+        formats: ['image/avif', 'image/webp'],
+    },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: ['@svgr/webpack'],
+        });
+
+        return config;
+    },
+};
 
 export default nextConfig;
